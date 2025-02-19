d4d219659947dbeacf9907797d7129197fe83fa2
ikrnumaC <beamsellar@gmail.com>
2025-02-19 09:57:35 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import CardList from '@/components/CardList'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <h1 className="text-2xl font-bold text-center py-4">メルカリSC</h1>
+    <main className="min-h-screen">
+      <h1 className="text-2xl font-bold text-center py-4 w-[1000px] mx-auto">メルカリSC</h1>
       <CardList />
     </main>
   )
