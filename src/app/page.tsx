71caa6493c66df02fa7e01d60f507da04e0a80a7
ikrnumaC <beamsellar@gmail.com>
2025-02-19 10:06:54 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import CardList from '../components/CardList'
+import CardList from './components/CardList'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <h1 className="text-2xl font-bold text-center py-4 w-[1000px] mx-auto">メルカリSC</h1>
-      <CardList />
-    </main>
-  )
+  return <CardList />
 }
